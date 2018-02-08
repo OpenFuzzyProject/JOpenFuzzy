8d0f2bc34f7ff7be61aa25224558808c82d1dfcb
Toru Ikeda <toru48@outlook.jp>
2018-02-08 18:52:41 +0900

    Add new shape fuzzy sets.

---
+++ src/impl/org/openfuzzy/fuzzy/set/SingletonShapeFuzzySet.java
@@ -0,0 +1,38 @@
+package org.openfuzzy.fuzzy.set;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.openfuzzy.fuzzy.lang.FuzzyLogic;
+
+public class SingletonShapeFuzzySet extends FuzzySetImpl implements IFuzzySet {
+
+	private String name;
+	private List<String> paramNames;
+	private double x1;
+
+	public SingletonShapeFuzzySet(String name, String axisName, double x) {
+		this.name = name;
+		this.paramNames = Arrays.asList(new String[] { axisName });
+		this.x1 = x;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public List<String> getParameterNames() {
+		return paramNames;
+	}
+
+	@Override
+	public IMembershipFunction getMembershipFunction() {
+		return input -> {
+			double x = input.get(paramNames.get(0));
+			return FuzzyLogic.get(x == x1);
+		};
+	}
+
+}
