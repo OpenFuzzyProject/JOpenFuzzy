268eae00c3e1f80f5b535a3cbe272607bd916616
Toru <toru48@outlook.jp>
2018-02-24 18:33:27 +0900

    add javadoc comment

--- src/impl/org/openfuzzy/fuzzy/util/ImportSamplePoints.java
+++ src/impl/org/openfuzzy/fuzzy/util/ImportSamplePoints.java
@@ -8,7 +8,42 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * An Importer of the files written sample points for multi-dimensional fuzzy
+ * set.
+ * 
+ * @author Toru Ikeda
+ *
+ */
 public class ImportSamplePoints {
+
+	// singleton class
+	private ImportSamplePoints() {
+	}
+
+	/**
+	 * <p>
+	 * Read TSV file written sample points without header.
+	 * 
+	 * <p>
+	 * It is need to put 1 sample point into 1 line like the follow.<br>
+	 * 
+	 * <pre>
+	 * 36\t70\n
+	 * 39\t60\n
+	 * 39\t80\n
+	 * </pre>
+	 * 
+	 * The each number strings  read from file are cast to Double number.
+	 * So the string format have to be able to cast by Double::valueOf.
+	 * 
+	 * @see java.lang.Double
+	 * 
+	 * @param file
+	 *            TSV file written sample points
+	 * 
+	 * @return sample points list
+	 */
 	public static List<double[]> fromTSV(File file) {
 		List<double[]> samplePoints = null;
 		try {
