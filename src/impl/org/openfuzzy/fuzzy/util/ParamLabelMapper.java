f90826db97f74d483799beadac53a65d12214bed
Toru <toru48@outlook.jp>
2018-05-20 22:09:55 +0900

    Add parameter label mapper

---
+++ src/impl/org/openfuzzy/fuzzy/util/ParamLabelMapper.java
@@ -0,0 +1,18 @@
+package org.openfuzzy.fuzzy.util;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ParamLabelMapper {
+	public static Map<String, Double> toMap(List<String> paramNames, double[] data) {
+		if(paramNames.size() != data.length)
+			throw new RuntimeException("The number of names or the length of data is wrong. : " + paramNames.toString() + " : " + Arrays.toString(data));
+		int s = paramNames.size();
+		Map<String, Double> map = new HashMap<>(s);
+		for(int i = 0; i < s; i++) 
+			map.put(paramNames.get(i), data[i]);
+		return map;
+	}
+}
